9fd23a35c5948ff79989269e1c7a29c431fd98a3
Jeremia Pane <jeripane@gmail.com>
2022-06-13 16:49:26 +0700

    Add comment

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+//menetapkan variabel calculatorScreen
 const calculatorScreen = document.querySelector('.calculator-screen');
-
+//Fungsi untuk menampilkan angka yang di tekan dan hasil perhitungan
 const updateScreen = (number) => {
     calculatorScreen.value = number;
 }
 
+//menetapkan variabel numbers
 const numbers = document.querySelectorAll(".number");
 
+//fungsi ketika angka ditekan
 numbers.forEach((number) => {
     number.addEventListener("click", (event) => {
         inputNumber(event.target.value);
@@ -13,11 +16,12 @@ numbers.forEach((number) => {
     })
 })
 
-
+//menetapkan variabel 
 let prevNumber = '';
 let calculationOperator = '';
 let currentNumber = '0';
 
+//fungsi yang menetapkan angka yang ditekan
 const inputNumber = (number) => {
     if (currentNumber === '0') {
         currentNumber = number;
@@ -26,6 +30,8 @@ const inputNumber = (number) => {
     }
 }
 
+
+//fungsi yang menetapkan elemen dengan kelas operator sebagai operator hitung dalam kalkulator
 const operators = document.querySelectorAll('.operator');
 operators.forEach((operator) => {
     operator.addEventListener("click", (event) => {
@@ -33,6 +39,7 @@ operators.forEach((operator) => {
     })
 })
 
+//fungsi yang menggantikan currentNumber menjadi value dari operator
 const inputOperator = (operator) => {
     if (calculationOperator === '') {
         prevNumber = currentNumber;
@@ -41,13 +48,16 @@ const inputOperator = (operator) => {
     currentNumber = '';
 }
 
+//menetapkan variabel = dari elemen dengan kelas "equal-sign" 
 const equalSign = document.querySelector('.equal-sign');
 
+//memberikan event/task ketika tombol = ditekan
 equalSign.addEventListener('click', () => {
     calculate();
     updateScreen(currentNumber);
 })
 
+//fungsi hitung
 const calculate = () => {
     let result = '';
     switch (calculationOperator) {
@@ -70,24 +80,33 @@ const calculate = () => {
     calculationOperator = '';
 }
 
+//menetapkan variabel hapus 
 const clearBtn = document.querySelector('.all-clear');
+
+//fungsi ketika tombol AC di tekan
 clearBtn.addEventListener('click', () => {
     clearAll();
     updateScreen(currentNumber);
 })
 
+//fungsi untuk hapul semua nilai 
 const clearAll = () => {
     prevNumber = '';
     calculationOperator = '';
     currentNumber = '0';
 }
 
+//menetapkan variabel decimal
 const decimal = document.querySelector('.decimal');
+
+//fungsi yang dijalankan ketika tombol . di tekan
 decimal.addEventListener('click', (event) => {
     inputDecimal(event.target.value)
     updateScreen(currentNumber);
 })
 
+
+//fungsi hitungan .
 inputDecimal = (dot) => {
 
     if (currentNumber.includes('.')) {
